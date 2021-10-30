7127621d442b9e20a94b3e13f6085d5125b38ad9
syed omer farooq <syedomer.cse@gmail.com>
2021-10-30 22:47:47 +0530

    added insert a node into the tree

---
+++ trees/5_InsertElement.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    // TreeNode(int x): val(x), left(NULL), right(NULL) {}
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void print_inorder(TreeNode *root) {
+    if(root != nullptr) {
+        print_inorder(root->left);
+        cout<<root->val<< " ";
+        print_inorder(root->right);
+    }
+}
+
+void insert(TreeNode *root, int value) {
+    TreeNode *temp = nullptr;
+    queue<TreeNode *>q;
+    q.push(root);
+    while(!q.empty()) {
+        temp = q.front();
+        q.pop();
+        if(temp->left)
+            q.push(temp->left);
+        if(!temp->left){
+            temp->left = new TreeNode(value);
+            return;
+        }
+        if(temp->right)
+            q.push(temp->right);
+        if(!temp->right) {
+            temp->right = new TreeNode(value);
+            return;
+        }
+    }
+}
+
+int main() {
+    TreeNode *root=new TreeNode(17);
+    root->left=new TreeNode(41);
+    root->right=new TreeNode(9);
+    root->left->left=new TreeNode(29);
+    root->left->right=new TreeNode(6);
+    root->right->left=new TreeNode(81);
+    root->right->right=new TreeNode(40);
+    root->right->right->right=new TreeNode(121);
+    print_inorder(root);
+    cout<<endl;
+    insert(root,111);
+    print_inorder(root);
+    cout<<endl;
+    return 0;
+}
