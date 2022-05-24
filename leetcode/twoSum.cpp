1fa371868be6a9beb9629305857f492854e344db
Syed Omer Farooq <syedomer.cse@gmail.com>
2022-05-24 19:50:55 +0530

    added optimal solution to two sum problem

--- leetcode/twoSum.cpp
+++ leetcode/twoSum.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> twoSumBruteForce(vector<int>& nums, int target) {
+        int temp = 0;
+        for(int i=0; i< nums.size(); i++) {
+            for(int j=i+1; j< nums.size(); j++) {
+                if(nums[i]+nums[j] == target) {
+                    return vector<int>{i, j};// can use this as well {i,j}
+                    break;
+                }
+            }
+        }
+        return vector<int>{0,0};// can use this as well {}
+    }
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+    for(int i=0; i<nums.size(); i++) {
+        if(map.find(target-nums[i]) != map.end()){
+            return {i, map[target-nums[i]]};
+        }
+        map[nums[i]] = i;
+    }
+     return {};
+}
 int main() {
     return 0;
 }
